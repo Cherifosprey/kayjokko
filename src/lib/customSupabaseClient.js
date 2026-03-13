45879114cd6b1f39ec4e44dc8b04ea56002d68db
unknown <cherifmoutvlib@gmail.com>
2026-03-13 21:44:10 +0000

    first commit

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,13 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://oyjhmzrrsgdmfjarisfc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95amhtenJyc2dkbWZqYXJpc2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NzA3OTcsImV4cCI6MjA4MDM0Njc5N30.KymIz72mC-4gJXjbdceWfMSvcrRKLNwqoAJ7XD7OuIw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
-
-export default customSupabaseClient;
-
-export { 
-    customSupabaseClient,
-    customSupabaseClient as supabase,
-};
+export const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
